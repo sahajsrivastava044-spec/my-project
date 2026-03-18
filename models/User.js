711690002a41e6cf2c0fff03137a6bbd64602070
sahajsrivastava044-spec <sahajsrivastava044@gmail.com>
2026-03-18 15:43:47 +0530

    Authentication and authorization middleware added

--- models/User.js
+++ models/User.js
@@ -8,17 +8,40 @@ const userSchema=new mongoose.Schema({
     },
     email:{
         type:String,
-        reuired:true,
+        required:true,
         unique:true
     },
-    passweord:{
+    password:{
         type:String,
-        reuired:true
+        required:true
+    },
+    comparePasswords:{
+        type:String,
+        // required:true
     },
     likedSongs:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Song'
-    }]
-})
+    }],
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'         
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports=mongoose.model('User',userSchema);
